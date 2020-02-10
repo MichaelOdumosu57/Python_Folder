bbc05d82c316829e85bc897c11d4a9406d7edefd
Michael Odumosu <michaelodumosu57@gmail.com>
2020-02-09 22:02:51 -0500

    learning about python3.8 strings

---
+++ Python_Folder_38/python38_learning.py
@@ -0,0 +1,458 @@
+
+                  .,:;i1ttttfffffttt1i;:.                   
+                .;1tfffffffffffffffffffft1:.                
+               ,1fft;::itffffffffffffffffff1.               
+               :fft:    ifffffffffffffffffff;               
+               :fff1,..:tfffffffffffffffffffi               
+               :fffffttfffffffffffffffffffffi               
+               ,iiiiiiiiiiiii1tfffffffffffffi               
+      ..,,,,,,,,,,,,,,,,,,,,,:tfffffffffffffi .,,,,,,..     
+   .:i1ttttttttttttfffffffffffffffffffffffffi ,;;;;;;;;:,   
+  :1tfffffffffffffffffffffffffffffffffffffffi ,;;;;;;;;;;:. 
+ :tfffffffffffffffffffffffffffffffffffffffffi ,;;;;;;;;;;;: 
+,tffffffffffffffffffffffffffffffffffffffffff; ,;;;;;;;;;;;;,
+iffffffffffffffffffffffffffffffffffffffffff1. :;;;;;;;;;;;;;
+tffffffffffffffffffffffffffffffffffffffff1:. ,;;;;;;;;;;;;;;
+ffffffffffffffffft1ii;;;;;;;;;;;;;;;;;::...,:;;;;;;;;;;;;;;;
+fffffffffffffff1:....,,,,,,,,,,,,,,,,,,,::;;;;;;;;;;;;;;;;ii
+tfffffffffffffi. .::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiii
+ifffffffffffft. ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiii;
+,tfffffffffffi .;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiiii;,
+ ;fffffffffffi ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiiiiii: 
+  ;tfffffffffi ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiiiiii;,  
+   ,itfffffffi ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:,.   
+     .,::::::, ,;;;;;;;;;;;;;;,......................       
+               ,;;;;;;;;;;;;;;::::::::::::::,               
+               ,;;;;;;;;;;;;;;;;;;;;;;;;;iii:               
+               ,;;;;;;;;;;;;;;;;;;;;,...:;ii:               
+               .;;;;;;;;;;;;;;;;;;;;    .;ii:               
+                ,;;;;;;;;;;;;;;;;;;;:,,,;ii;,               
+                 .:;;;;;;;;;;;;;;iiiiiii;;:.                
+                    .,:::;;;;;;;;;;;;::,. 
+
+
+
+
+                                          .::;i1t;
+                                       ,:ifffft1;.
+                            :i1fLttt11fLfft11:.   
+                          ,fGGCGCCGGCCf1tLfi.     
+                         :GCLGGGffffCCt;fLt:      
+                         fCLCC1.    .:1tf1ii,     
+                        :CfLGG:        .:i1i::..  
+                        ,CCLCC1.          .,::;;. 
+                         18GLLCf:.                
+                          tGCLLGLf;,.             
+                           ;C80LLCCft;,.          
+                             ;LGCLLLCL1t;,.       
+                          :1tttC0880GLLL1;;;.     
+                         1G0GGGCG08@0GGCGL1i1:    
+                        iG0GG00LLLCG8800GGCtLt,   
+     .;1tLfft11;:;;:::,,f080000GCCGCCG080GGCf1;   
+   ,tCGG088GG00GLGGLCCLLLCCGGGGGGGGCLCGG00G0L1,   
+  ;GGGC08880000G088GG0G0GGGGCCCCCCCCCG8GG00Gt;    
+ :00GC0L10000GGGGCC08880GGCGGGGGGCCCG080GGGt:     
+.fC008C .CG0080CCLLC0880GCLLLLLCCGGGCLC0GL:.      
+,CLCGGfi1fLLCGCLGGCG80080GGCG0GC08000GfLGt,..     
+.fGGCLC00GLL00GC00GGG00@@80GGGGGGGGGGGGLL;,.      
+ :LCGGCCG0GCG0GCGGGGGGCG0GCCCCGGGGGGCLLt;..       
+  .;1tfLCCCGGGCCCCLft;:,,....,,:::::,.            
+      ..,::::::,,.. 
+
+                .:,..,:.                
+                i1i::i1i                
+           .:;1tf1,  ,1ft1;:.           
+        ,;1fLLLL1;i  i;1LLLLf1;,        
+      :1fLLLft1;:,;::;,:;1tfLLLf1:      
+    .1Lffff1;i111.    .111i;1ffffL1.    
+   .tLfff1:i1111:      :1111i:1fffLt.   
+   iLffft,11111i        i11111,tfffLi   
+   iLffft,11111;        ;11111,tfffLi   
+   ,fLffLi,i111i        i111i,iLffLf,   
+    ,tLLfft:;i11,      ,11i;:tffLLt,    
+      :1ffLLfi;ii      ii;ifLLff1:      
+        .,;1fLLti:    :itLLf1;,.        
+             ,:1ft.  .tf1:,             
+                .11  11.                
+                  ;  ;          
+
+
+        ,i;i:.                          
+     .:ii, ;ft11i:                      
+   ,itttCi:11tfCGCt:                    
+  :Lfft11i1i:11i1tLC:                   
+  .,::;i:::,,, .;fGC, .:1fft1,          
+ .:i::i,   ..,;tft;..1LCGCCCG;          
+ .i;.,,..,:1ft11:  ifCGf;iLGt           
+     .,;tft1i,   :LGCf1:1Cf: .;11,      
+   .:itfL1,    ;LCLLt;iLL; .1CCLC.      
+  .:iLCG;  .:1fLGCf::1Ct  ;C0f:t1    ,, 
+ .,;1LLCLfLLCGGLt; .1CG;;LGC1 .1f;:;;:. 
+   .;tLCGGCCLf1,   ,;fCCGC1,   .,::.    
+     .:;ii;:,      . .:;:.                                                                    
+                               
+                                
+            .tt:,.                         ;1CLff1i.        
+         .:1fLG8Cf;                       1L1fffti..,       
+       .ifft1fLL1tC.                    ,10CC;itt:;;,       
+       ,iitftt,,ift.                      tCL1.:,.   ..     
+      .::ii1Lt;tti.   .:i.,. ,tt:          ;11i1ttfLCCi     
+      :tfftfftL1      .1LfCf1: 1t          ,tLt1tiiGLCCL;   
+   ,ifGft1iftLfLf,   ..,1fLCLiif;;,      ,fCLtLtt: :,tLGGt  
+   10CC0t. .:1ttCt  ;Lf1fffCfLCftCG;     fLLL1t1;.  .tfLG0; 
+  ;CLftt:  .:fft1, :fCCLt1i1LLffffLt;:,  1GLLt1:     ,ifLC: 
+ ,CLLLLf. .fLfC1  :LLt1ftiitf11fLLffLft;  ;tff11;      ;ff, 
+ .i1;1LL.  fL1ft.ifC1.fttt1tftfLGCfL111i::,::iCt11:     .i: 
+    ,ff:   LtittitCGi ,t1itLLCCLf. fCf1ittfCLfCLt1t:     .  
+    ti.    iLfftttC; .;tLiiftLCi.  ,tfftfCCCCCftftt:        
+    ..      ,i1fff;  .LGGLCCCC0G,    .,.,:;;iii11i:         
+               ...   ifCGCLfffCGf.                          
+                    ,t1tLfttfffGG:         ,;iii;:.         
+                    itfLfffftttLCL.     :1fftiiii1f:        
+                   :tfC0Ltfftt11LGL..:1LCt:.      ;i        
+                   ifLL1::tft11fLGCCCGL1,         ,.        
+                   iLC1,1t1tLCLCGi:.                     
+                   .tft,;i;;tGCi.                      
+
+are tips 
+          .:,.,,    
+         .i1tt1t:   
+         i1tfLtft   
+        ,ffffffff.  
+        .1LCLffti   
+          :fLf:.  \    
+   .       :LL;    \ 
+  :,        :Lf:    
+  ;;,,..     1L1    
+  .,:;iiii;:.:t1.   
+       ..:ifLt11.   
+           .;Lt1,   
+             ,ii.  
+
+
+
+
+                  .,:;i1ttttfffffttt1i;:.                   
+                .;1tfffffffffffffffffffft1:.                
+               ,1fft;::itffffffffffffffffff1.               
+               :fft:    ifffffffffffffffffff;               
+               :fff1,..:tfffffffffffffffffffi               
+               :fffffttfffffffffffffffffffffi               
+               ,iiiiiiiiiiiii1tfffffffffffffi               
+      ..,,,,,,,,,,,,,,,,,,,,,:tfffffffffffffi .,,,,,,..     
+   .:i1ttttttttttttfffffffffffffffffffffffffi ,;;;;;;;;:,   
+  :1tfffffffffffffffffffffffffffffffffffffffi ,;;;;;;;;;;:. 
+ :tfffffffffffffffffffffffffffffffffffffffffi ,;;;;;;;;;;;: 
+,tffffffffffffffffffffffffffffffffffffffffff; ,;;;;;;;;;;;;,
+iffffffffffffffffffffffffffffffffffffffffff1. :;;;;;;;;;;;;;
+tffffffffffffffffffffffffffffffffffffffff1:. ,;;;;;;;;;;;;;;
+ffffffffffffffffft1ii;;;;;;;;;;;;;;;;;::...,:;;;;;;;;;;;;;;;
+fffffffffffffff1:....,,,,,,,,,,,,,,,,,,,::;;;;;;;;;;;;;;;;ii
+tfffffffffffffi. .::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiii
+ifffffffffffft. ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiii;
+,tfffffffffffi .;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiiii;,
+ ;fffffffffffi ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiiiiii: 
+  ;tfffffffffi ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;iiiiiiii;,  
+   ,itfffffffi ,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:,.   
+     .,::::::, ,;;;;;;;;;;;;;;,......................       
+               ,;;;;;;;;;;;;;;::::::::::::::,               
+               ,;;;;;;;;;;;;;;;;;;;;;;;;;iii:               
+               ,;;;;;;;;;;;;;;;;;;;;,...:;ii:               
+               .;;;;;;;;;;;;;;;;;;;;    .;ii:               
+                ,;;;;;;;;;;;;;;;;;;;:,,,;ii;,               
+                 .:;;;;;;;;;;;;;;iiiiiii;;:.                
+                    .,:::;;;;;;;;;;;;::,. 
+
+
+
+Python 3
+
+
+                                          .::;i1t;
+                                       ,:ifffft1;.
+                            :i1fLttt11fLfft11:.   
+                          ,fGGCGCCGGCCf1tLfi.     
+                         :GCLGGGffffCCt;fLt:      
+                         fCLCC1.    .:1tf1ii,     
+                        :CfLGG:        .:i1i::..  
+                        ,CCLCC1.          .,::;;. 
+                         18GLLCf:.                
+                          tGCLLGLf;,.             
+                           ;C80LLCCft;,.          
+                             ;LGCLLLCL1t;,.       
+                          :1tttC0880GLLL1;;;.     
+                         1G0GGGCG08@0GGCGL1i1:    
+                        iG0GG00LLLCG8800GGCtLt,   
+     .;1tLfft11;:;;:::,,f080000GCCGCCG080GGCf1;   
+   ,tCGG088GG00GLGGLCCLLLCCGGGGGGGGCLCGG00G0L1,   
+  ;GGGC08880000G088GG0G0GGGGCCCCCCCCCG8GG00Gt;    
+ :00GC0L10000GGGGCC08880GGCGGGGGGCCCG080GGGt:     
+.fC008C .CG0080CCLLC0880GCLLLLLCCGGGCLC0GL:.      
+,CLCGGfi1fLLCGCLGGCG80080GGCG0GC08000GfLGt,..     
+.fGGCLC00GLL00GC00GGG00@@80GGGGGGGGGGGGLL;,.      
+ :LCGGCCG0GCG0GCGGGGGGCG0GCCCCGGGGGGCLLt;..       
+  .;1tfLCCCGGGCCCCLft;:,,....,,:::::,.            
+      ..,::::::,,.. 
+
+
+Installing python 3 side by side for windows
+
+        download Windows x86-64 web-based installer python3.8
+        custom install  
+        im getting the documentation file 
+        pip 
+        Python test suite 
+        py launcher for all users
+
+        install location 
+        C:\Python38
+
+        sucessful 
+
+        i went and disabled PATH limit 
+
+        added C:/Python38/
+        added  C:/Python38/Scripts/
+        to env varss
+        restart computer
+        were good to go 
+
+
+
+                                          .::;i1t;
+                                       ,:ifffft1;.
+                            :i1fLttt11fLfft11:.   
+                          ,fGGCGCCGGCCf1tLfi.     
+                         :GCLGGGffffCCt;fLt:      
+                         fCLCC1.    .:1tf1ii,     
+                        :CfLGG:        .:i1i::..  
+                        ,CCLCC1.          .,::;;. 
+                         18GLLCf:.                
+                          tGCLLGLf;,.             
+                           ;C80LLCCft;,.          
+                             ;LGCLLLCL1t;,.       
+                          :1tttC0880GLLL1;;;.     
+                         1G0GGGCG08@0GGCGL1i1:    
+                        iG0GG00LLLCG8800GGCtLt,   
+     .;1tLfft11;:;;:::,,f080000GCCGCCG080GGCf1;   
+   ,tCGG088GG00GLGGLCCLLLCCGGGGGGGGCLCGG00G0L1,   
+  ;GGGC08880000G088GG0G0GGGGCCCCCCCCCG8GG00Gt;    
+ :00GC0L10000GGGGCC08880GGCGGGGGGCCCG080GGGt:     
+.fC008C .CG0080CCLLC0880GCLLLLLCCGGGCLC0GL:.      
+,CLCGGfi1fLLCGCLGGCG80080GGCG0GC08000GfLGt,..     
+.fGGCLC00GLL00GC00GGG00@@80GGGGGGGGGGGGLL;,.      
+ :LCGGCCG0GCG0GCGGGGGGCG0GCCCCGGGGGGCLLt;..       
+  .;1tfLCCCGGGCCCCLft;:,,....,,:::::,.            
+      ..,::::::,,.. 
+
+Interactive shell 
+
+
+to start  
+python38 
+
+to exit 
+(Control-D on Unix, Control-Z on Windows)
+then enter 
+
+
+
+default decoding 
+ASCII
+
+TO change the encoding
+# -*- coding: [encoding] -*-
+
+                                          .::;i1t;
+                                       ,:ifffft1;.
+                            :i1fLttt11fLfft11:.   
+                          ,fGGCGCCGGCCf1tLfi.     
+                         :GCLGGGffffCCt;fLt:      
+                         fCLCC1.    .:1tf1ii,     
+                        :CfLGG:        .:i1i::..  
+                        ,CCLCC1.          .,::;;. 
+                         18GLLCf:.                
+                          tGCLLGLf;,.             
+                           ;C80LLCCft;,.          
+                             ;LGCLLLCL1t;,.       
+                          :1tttC0880GLLL1;;;.     
+                         1G0GGGCG08@0GGCGL1i1:    
+                        iG0GG00LLLCG8800GGCtLt,   
+     .;1tLfft11;:;;:::,,f080000GCCGCCG080GGCf1;   
+   ,tCGG088GG00GLGGLCCLLLCCGGGGGGGGCLCGG00G0L1,   
+  ;GGGC08880000G088GG0G0GGGGCCCCCCCCCG8GG00Gt;    
+ :00GC0L10000GGGGCC08880GGCGGGGGGCCCG080GGGt:     
+.fC008C .CG0080CCLLC0880GCLLLLLCCGGGCLC0GL:.      
+,CLCGGfi1fLLCGCLGGCG80080GGCG0GC08000GfLGt,..     
+.fGGCLC00GLL00GC00GGG00@@80GGGGGGGGGGGGLL;,.      
+ :LCGGCCG0GCG0GCGGGGGGCG0GCCCCGGGGGGCLLt;..       
+  .;1tfLCCCGGGCCCCLft;:,,....,,:::::,.            
+      ..,::::::,,.. 
+
+
+starting python 3.8
+
+                .:,..,:.                
+                i1i::i1i                
+           .:;1tf1,  ,1ft1;:.           
+        ,;1fLLLL1;i  i;1LLLLf1;,        
+      :1fLLLft1;:,;::;,:;1tfLLLf1:      
+    .1Lffff1;i111.    .111i;1ffffL1.    
+   .tLfff1:i1111:      :1111i:1fffLt.   
+   iLffft,11111i        i11111,tfffLi   
+   iLffft,11111;        ;11111,tfffLi   
+   ,fLffLi,i111i        i111i,iLffLf,   
+    ,tLLfft:;i11,      ,11i;:tffLLt,    
+      :1ffLLfi;ii      ii;ifLLff1:      
+        .,;1fLLti:    :itLLf1;,.        
+             ,:1ft.  .tf1:,             
+                .11  11.                
+                  ;  ;          
+
+
+Comments
+# this is the first comment
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+                .:,..,:.                
+                i1i::i1i                
+           .:;1tf1,  ,1ft1;:.           
+        ,;1fLLLL1;i  i;1LLLLf1;,        
+      :1fLLLft1;:,;::;,:;1tfLLLf1:      
+    .1Lffff1;i111.    .111i;1ffffL1.    
+   .tLfff1:i1111:      :1111i:1fffLt.   
+   iLffft,11111i        i11111,tfffLi   
+   iLffft,11111;        ;11111,tfffLi   
+   ,fLffLi,i111i        i111i,iLffLf,   
+    ,tLLfft:;i11,      ,11i;:tffLLt,    
+      :1ffLLfi;ii      ii;ifLLff1:      
+        .,;1fLLti:    :itLLf1;,.        
+             ,:1ft.  .tf1:,             
+                .11  11.                
+                  ;  ;          
+
+
+
+>>> 8 / 5  # division always returns a floating point number
+1.6
+
+
+>>> 17 // 3  # floor division discards the fractional part
+5
+
+In interactive mode, the last printed expression is assigned to the variable _.
+>>> tax = 12.5 / 100
+>>> price = 100.50
+>>> price * tax
+12.5625
+>>> price + _
+113.0625
+>>> round(_, 2)
+113.06
+
+
+                .:,..,:.                
+                i1i::i1i                
+           .:;1tf1,  ,1ft1;:.           
+        ,;1fLLLL1;i  i;1LLLLf1;,        
+      :1fLLLft1;:,;::;,:;1tfLLLf1:      
+    .1Lffff1;i111.    .111i;1ffffL1.    
+   .tLfff1:i1111:      :1111i:1fffLt.   
+   iLffft,11111i        i11111,tfffLi   
+   iLffft,11111;        ;11111,tfffLi   
+   ,fLffLi,i111i        i111i,iLffLf,   
+    ,tLLfft:;i11,      ,11i;:tffLLt,    
+      :1ffLLfi;ii      ii;ifLLff1:      
+        .,;1fLLti:    :itLLf1;,.        
+             ,:1ft.  .tf1:,             
+                .11  11.                
+                  ;  ;     
+
+strings                   
+
+>>> 'spam eggs'  # single quotes
+'spam eggs'
+>>> 'doesn\'t'  # use \' to escape the single quote...
+"doesn't"
+>>> "doesn't"  # ...or use double quotes instead
+"doesn't"
+>>> '"Yes," they said.'
+'"Yes," they said.'
+>>> "\"Yes,\" they said."
+'"Yes," they said.'
+>>> '"Isn\'t," they said.'
+'"Isn\'t," they said.'
+
+
+>>> print('C:\some\name')  # here \n means newline!
+C:\some
+ame
+>>> print(r'C:\some\name')  # note the r before the quote
+C:\some\name
+
+
+multi line 
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+
+use + and * w/ strings
+>>> # 3 times 'un', followed by 'ium'
+>>> 3 * 'un' + 'ium'
+'unununium'
+
+
+also to concatenate strings 
+but does not work with string objects
+>>> 'Py' 'thon'
+'Python'
+
+to count in reverse
+word[-1]  # last character
+
+to substring 
+>>> word[2:5]  # characters from position 2 (included) to 5 (excluded)
+'tho'
+
+substring features 
+>>> word[:2] + word[2:]
+'Python'
+>>> word[:4] + word[4:]
+'Python'
+>>> word[4:]   # characters from position 4 (included) to the end
+'on'
+>>> word[-2:]  # characters from the second-last (included) to the end
+'on'
+
+out of range slices handled gracefully 
+>>> word[4:42]
+'on'
+>>> word[42:]
+''
+
+
+          .:,.,,    
+         .i1tt1t:   
+         i1tfLtft   
+        ,ffffffff.  
+        .1LCLffti   
+          :fLf:.  \    
+   .       :LL;    \    python strings are immutable 
+  :,        :Lf:    
+  ;;,,..     1L1    
+  .,:;iiii;:.:t1.   
+       ..:ifLt11.   
+           .;Lt1,   
+             ,ii.  
+
+
+The built-in function len() returns the length of a string:
+>>>
+>>> s = 'supercalifragilisticexpialidocious'
+>>> len(s)
+34
+
